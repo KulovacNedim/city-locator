4ebf969ee2a6eecdb2434ec4f094c14a14415b64
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-14 21:55:59 +0100

    first commit

---
+++ src/main/java/dev/ned/city/locator/CityLocatorApplication.java
@@ -0,0 +1,13 @@
+package dev.ned.city.locator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityLocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityLocatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ned/city/locator/CityLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ned.city.locator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityLocatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
