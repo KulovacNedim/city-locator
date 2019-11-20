6b5b6d727876a3b0be4ddda87a06fe7ed71a07d2
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-20 12:59:17 +0100

    cors tunning

--- src/main/java/dev/ned/city/locator/rest_controllers/LocationRestController.java
+++ src/main/java/dev/ned/city/locator/rest_controllers/LocationRestController.java
@@ -11,7 +11,6 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
-//@CrossOrigin(origins = {"/**"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api/location")
 public class LocationRestController {
