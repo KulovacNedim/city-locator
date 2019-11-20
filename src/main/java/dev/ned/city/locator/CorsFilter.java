8da37803b5436c98c7d67298ce0e05fde345666d
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-20 21:40:43 +0100

    cors configured to allow access from heroku as well as from local server

--- src/main/java/dev/ned/city/locator/CorsFilter.java
+++ src/main/java/dev/ned/city/locator/CorsFilter.java
@@ -1,12 +1,9 @@
 package dev.ned.city.locator;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -19,9 +16,12 @@ import java.io.IOException;
 @Component
 public class CorsFilter implements Filter {
 
+    @Value( "${cors.origin}" )
+    private String corsOrigin;
+
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "https://city-locator-ui.herokuapp.com");
+        response.setHeader("Access-Control-Allow-Origin", corsOrigin);
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
